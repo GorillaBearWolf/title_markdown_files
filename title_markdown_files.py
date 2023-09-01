c210079e0f53092339357de00b593dc99cd2d9e8
Matthew Refsnider <mrefsnider@icloud.com>
2023-09-01 15:17:54 -0400

    Update names

--- title_markdown_files.py
+++ title_markdown_files.py
@@ -5,7 +5,7 @@ list_of_files = os.listdir(folder_path)
 
 for file in list_of_files:
     if file.endswith(".md") and not file.startswith("."):
-        old_path = os.path.join(folder_path, file)
-        new_name = file.removesuffix(".md").replace("-", " ").replace("_", " ").title().strip() + ".md"
-        new_path = os.path.join(folder_path, new_name)
-        os.rename(old_path, new_path)
+        new_file = file.removesuffix(".md").replace("-", " ").replace("_", " ").title().strip() + ".md"
+        old_name = os.path.join(folder_path, file)
+        new_name = os.path.join(folder_path, new_file)
+        os.rename(old_name, new_name)
