59ffc11e7b50a26fb2260b84339e6b6740e3bd24
Matthew Refsnider <mrefsnider@icloud.com>
2023-09-01 14:13:10 -0400

    Initial commit

---
+++ test_title_markdown_files/test_title_markdown_files.sh
@@ -0,0 +1,24 @@
+﻿#!/bin/bash 2> /dev/null
+
+OLD_FILES=(
+    "Test-Markdown_File.md"
+    "Test Markdown File.md"
+    ".Test-Markdown_File.md"
+    ".Test Markdown File.md"
+    )
+NEW_FILES=(
+    "Test-Markdown_File.md"
+    ".Test-Markdown_File.md"
+    )
+
+for i in "${OLD_FILES[@]}"
+do
+    rm -f "$i"
+done
+
+for i in "${NEW_FILES[@]}"
+do
+    touch "$i"
+done
+
+python3 ../rename_markdown_files.py
---
+++ title_markdown_files.py
@@ -0,0 +1,11 @@
+﻿import os
+
+folder_path = input("Enter folder path: ")
+list_of_files = os.listdir(folder_path)
+
+for file in list_of_files:
+    if file.endswith(".md") and not file.startswith("."):
+        old_path = os.path.join(folder_path, file)
+        new_name = file.removesuffix(".md").replace("-", " ").replace("_", " ").title().strip() + ".md"
+        new_path = os.path.join(folder_path, new_name)
+        os.rename(old_path, new_path)
