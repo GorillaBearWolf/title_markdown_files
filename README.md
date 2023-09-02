768e3ffa219118ed07c8d51610a2cdab50416400
Matthew Refsnider <mrefsnider@icloud.com>
2023-09-01 21:35:17 -0400

    Expanded tests and updated readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A simple Python script that accepts a folder path _(enter a period ( . ) for current folder)_ and renames all non-hidden Markdown files in the folder, removing dashes ( - ) and underscores ( _ ) and transforming the string into title case.
 
 A test script is also included for validation.
+
+This **will** overwrite current files with the same name after the script runs.
--- test_title_markdown_files/test_title_markdown_files.sh
+++ test_title_markdown_files/test_title_markdown_files.sh
@@ -1,28 +1,24 @@
 ﻿#!/bin/bash 2> /dev/null
 
-OLD_FILES=(
-    "Test-Markdown_File.md"
+TEST_FILES=(
     "Test Markdown File.md"
-    ".Test-Markdown_File.md"
-    ".Test Markdown File.md"
-    "Test-Markdown_File.txt"
-    ".Test-Markdown_File.txt"
-    )
-NEW_FILES=(
     "Test-Markdown_File.md"
     ".Test-Markdown_File.md"
     "Test-Markdown_File.txt"
     ".Test-Markdown_File.txt"
     )
 
-for i in "${OLD_FILES[@]}"
-do
-    rm -f "$i"
-done
-
-for i in "${NEW_FILES[@]}"
+for i in "${TEST_FILES[@]}"
 do
     touch "$i"
+    echo "$i" | tee -a "$i" &> /dev/null
 done
 
 python3 ../title_markdown_files.py
+
+read -p "Press Enter to delete test files..."
+
+for i in "${TEST_FILES[@]}"
+do
+    rm -f "$i"
+done
